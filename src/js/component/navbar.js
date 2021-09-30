68c884d87726f90790aff248c4214a07a56d82f9
Angel Restifo <angelrestifo36@gmail.com>
2021-09-30 10:11:49 -0400

    Distribuidora hermanos Restifo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.scss";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-success mb-3 pt-3 pb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Distribuidora Hermanos Restifo</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,9 @@ import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>desde 1994</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
 	</div>
 );
